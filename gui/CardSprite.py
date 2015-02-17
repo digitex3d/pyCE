de29bdaece2f3787fd8deaa9c0ac677dc222fcfc
digitex3d <digitex3d@gmail.com>
2015-02-17 15:20:57 +0100

    Modification des commentaires

--- gui/CardSprite.py
+++ gui/CardSprite.py
@@ -1,4 +1,4 @@
-""" Cette classe représente le Sprite d'une carte affichable"""
+
 
 from environment import Card
 from pyglet.sprite import Sprite
@@ -6,6 +6,8 @@ import pyglet.image
 BACK_SIDE_IMAGE = pyglet.image.load("data/cards/b.gif")
 
 class CardSprite(Sprite):
+    """ Cette classe représente le Sprite d'une carte"""
+
     def __init__(self, card, img, x=0, y=0, blend_src=770, blend_dest=771, batch=None, group=None, usage='dynamic'):
         img = pyglet.image.load(img)
         super(CardSprite, self).__init__(img, x, y, blend_src,blend_dest, batch, group, usage)
@@ -13,8 +15,10 @@ class CardSprite(Sprite):
         self.side=1
         self.card_image = img
 
-    """ Permet de changer le coté affiché d'une carte """
+
     def switch_side(self):
+        """ Permet de changer le coté affiché d'une carte
+        """
         if( self.side == 1 ):
             self.side = 0
             self.image =  BACK_SIDE_IMAGE
